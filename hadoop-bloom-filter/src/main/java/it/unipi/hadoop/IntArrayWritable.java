6f6513df04e29929652455907ab1acd6636e0355
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2022-05-24 17:13:20 +0200

    Add get and write in IntArrayWritable

--- hadoop-bloom-filter/src/main/java/it/unipi/hadoop/IntArrayWritable.java
+++ hadoop-bloom-filter/src/main/java/it/unipi/hadoop/IntArrayWritable.java
@@ -3,8 +3,23 @@ package it.unipi.hadoop;
 import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.IntWritable;
 
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class IntArrayWritable extends ArrayWritable {
 	public IntArrayWritable() {
 		super(IntWritable.class);
 	}
+
+	@Override
+    public IntWritable[] get () {
+        return (IntWritable[]) super.get();
+    }
+
+    @Override
+    public void write (DataOutput dataOutput) throws IOException {
+        for(IntWritable data : get()){
+            data.write(dataOutput);
+        }
+    }
 }
