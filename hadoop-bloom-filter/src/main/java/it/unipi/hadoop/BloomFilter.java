c968de3703f401be213a9ec000ab184700c960a8
veronicator <80213270+veronicator@users.noreply.github.com>
2022-05-24 17:12:59 +0200

    added global configuration (to check)

--- hadoop-bloom-filter/src/main/java/it/unipi/hadoop/BloomFilter.java
+++ hadoop-bloom-filter/src/main/java/it/unipi/hadoop/BloomFilter.java
@@ -18,6 +18,36 @@ import org.apache.hadoop.util.GenericOptionsParser;
 
 public class BloomFilter {
 	public static void main (String[] args) throws Exception {
-		
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		if (otherArgs.length < 2)	// ??
+			System.err.println("Usage: BloomFilter [input] [output]"); System.exit(2);
+
+		// Job configuration
+		Job job = Job.getInstance(conf, "BloomFilter");
+		job.getConfiguration().set("bloom.filter.false", "value p");	// from cmd line
+		job.getConfiguration().set("bloom.filter.keys", "value n");		// to estimate
+		// compute m and k from expression in the guideline
+		job.getConfiguration().set("bloom.filter.size", "value m");	// to set correctly
+		job.getConfiguration().set("bloom.filter.hash", "value k");
+
+		job.setJarByClass(BloomFilter.class);
+		job.setMapperClass(BloomFilterMapper.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntArrayWritable.class);
+		//iteration.setCombinerClass(BloomFilterCombiner.class);
+		job.setReducerClass(BloomFilterReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntArrayWritable.class);
+
+		job.setNumReduceTasks(1);		// to set correctly
+
+		// Destination File -> to configure
+		FileInputFormat.addInputPath(job, new Path("INPUT"));
+		FileOutputFormat.setOutputPath(job, new Path("OUTPUT"));
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
