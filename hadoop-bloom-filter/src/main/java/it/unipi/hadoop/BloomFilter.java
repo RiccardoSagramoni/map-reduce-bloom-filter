2a59920482f470aeee4c555818eac359fc7ebc9b
veronicator <80213270+veronicator@users.noreply.github.com>
2022-05-24 17:13:52 +0200

    added global configuration (to check)

--- hadoop-bloom-filter/src/main/java/it/unipi/hadoop/BloomFilter.java
+++ hadoop-bloom-filter/src/main/java/it/unipi/hadoop/BloomFilter.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
