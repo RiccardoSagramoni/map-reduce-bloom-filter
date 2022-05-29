08cdcf27c5443dba6978be5b894d3acf564e5bc4
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2022-05-29 17:25:02 +0200

    [Tester] code cleaning

--- hadoop-bloom-filter/src/main/java/it/unipi/hadoop/tester/MapperDataset.java
+++
@@ -1,8 +0,0 @@
-package it.unipi.hadoop.tester;
-
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
-
-public class MapperDataset extends Mapper<LongWritable, Text, Text, Text> {
-}
