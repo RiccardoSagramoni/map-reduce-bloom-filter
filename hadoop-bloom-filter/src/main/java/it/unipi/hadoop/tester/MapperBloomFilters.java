87e16c2196587c1783e13126f098ce68d69e0512
Fabi8997 <fabianopilia@gmail.com>
2022-05-29 20:03:16 +0200

    Added map function to MapperBloomFilters (identity function)

--- hadoop-bloom-filter/src/main/java/it/unipi/hadoop/tester/MapperBloomFilters.java
+++ hadoop-bloom-filter/src/main/java/it/unipi/hadoop/tester/MapperBloomFilters.java
@@ -4,6 +4,13 @@ import it.unipi.hadoop.BooleanArrayWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class MapperBloomFilters extends Mapper<IntWritable, BooleanArrayWritable, IntWritable, BooleanArrayWritable> {
-	// TODO @Fabiano
+
+    @Override
+    protected void map(IntWritable key, BooleanArrayWritable value, Mapper<IntWritable, BooleanArrayWritable, IntWritable, BooleanArrayWritable>.Context context) throws IOException, InterruptedException {
+        super.map(key, value, context);
+    }
+
 }
