f8d72796251259bc654145f568a61566c1f8e2d8
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2022-06-16 12:07:35 +0200

    Optimize how reducer read the configuration param

--- hadoop-bloom-filter/src/main/java/it/unipi/hadoop/bloomfilter/builder/BloomFilterReducer.java
+++ hadoop-bloom-filter/src/main/java/it/unipi/hadoop/bloomfilter/builder/BloomFilterReducer.java
@@ -19,45 +19,19 @@ import java.io.IOException;
  */
 public class BloomFilterReducer extends Reducer<ByteWritable, IntArrayWritable, ByteWritable, BooleanArrayWritable>  {
 
-	// Size of the bloom filter (taken from mapreduce configuration)
-	private int[] BLOOM_FILTERS_SIZE;
 	// Writable array for the result of the reducer (i.e. the bloom filter)
 	private final BooleanArrayWritable SERIALIZABLE_BLOOM_FILTER = new BooleanArrayWritable();
 
-	@Override
-	public void setup (Context context) {
-		// Retrieve configuration
-		Configuration configuration = context.getConfiguration();
-
-		// Read how many bloom filters will be created and allocate the array for the sizes
-		int howManyBloomFilters = configuration.getInt("bloom.filter.number", -1);
-		if (howManyBloomFilters <= 0) {
-			System.err.println("[MAPPER]: bloom.filter.number parameter not set");
-			return;
-		}
-		BLOOM_FILTERS_SIZE = new int[howManyBloomFilters];
-
-		// Read how large each bloom filter must be
-		// and build the array BLOOM_FILTERS_SIZE
-		for (int i = 0; i < howManyBloomFilters; i++) {
-			int bloomFilterSize = configuration.getInt(
-					"bloom.filter.size." + i,
-					-1
-			);
-			if (bloomFilterSize <= 0) {
-				System.err.println("[MAPPER]: bloom.filter.hash." + i + " parameter not set");
-				return;
-			}
-			BLOOM_FILTERS_SIZE[i] = bloomFilterSize;
-		}
-
-	}
 
 	@Override
 	public void reduce (ByteWritable key, Iterable<IntArrayWritable> values, Context context)
 			throws IOException, InterruptedException
 	{
-		int bloomFilterSize = BLOOM_FILTERS_SIZE[key.get()];
+		// Get the size of current bloom filter
+		int bloomFilterSize = context.getConfiguration().getInt(
+				"bloom.filter.size." + key.get(),
+				-1
+		);
 
 		// Instantiate the temporary bloom filter, i.e. an array of BooleanWritable
 		BooleanWritable[] bloomFilter = new BooleanWritable[bloomFilterSize];
