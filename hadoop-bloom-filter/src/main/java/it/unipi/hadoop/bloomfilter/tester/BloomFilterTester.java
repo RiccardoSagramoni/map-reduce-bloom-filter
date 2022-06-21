92d6e2f7e2193c9ede9951f70416c5b23a8ff28a
veronicator <80213270+veronicator@users.noreply.github.com>
2022-06-21 13:40:22 +0200

    debug tester

--- hadoop-bloom-filter/src/main/java/it/unipi/hadoop/bloomfilter/tester/BloomFilterTester.java
+++ hadoop-bloom-filter/src/main/java/it/unipi/hadoop/bloomfilter/tester/BloomFilterTester.java
@@ -1,7 +1,11 @@
 package it.unipi.hadoop.bloomfilter.tester;
 
+import it.unipi.hadoop.bloomfilter.builder.BloomFilter;
 import it.unipi.hadoop.bloomfilter.builder.BloomFilterMapper;
+import it.unipi.hadoop.bloomfilter.builder.BloomFilterReducer;
+import it.unipi.hadoop.bloomfilter.writables.BooleanArrayWritable;
 import it.unipi.hadoop.bloomfilter.writables.GenericObject;
+import it.unipi.hadoop.bloomfilter.writables.IntArrayWritable;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.ByteWritable;
@@ -11,38 +15,106 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.NLineInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BloomFilterTester {
 
-	private static final int NUMBER_OF_REDUCERS = 1; // TODO set correctly
-	private static final int LINES_PER_MAP = 5; // TODO set correctly
+	private static final int NUMBER_OF_REDUCERS = 4;
+	private static final int LINES_PER_MAP = 10000;
+
+	/**
+	 * TODO
+	 * @param falsePositiveProbability
+	 * @return
+	 */
+	private static int computeNumberOfHashFunctions (double falsePositiveProbability) {
+		return (int)(Math.ceil(-(Math.log(falsePositiveProbability)/(Math.log(2)))));
+	}
 
 
-	public static void main (String[] args) 
-				throws IOException, InterruptedException, ClassNotFoundException
+	/**
+	 * TODO
+	 * @param falsePositiveProbability
+	 * @param numberOfInputs
+	 * @return
+	 */
+	private static int computeSizeOfBloomFilter (double falsePositiveProbability, int numberOfInputs) {
+		return (int)(Math.ceil(-(numberOfInputs * Math.log(falsePositiveProbability)) / (2 * (Math.log(2)))));
+	}
+
+	private static void generateConfiguration (Configuration configuration,
+											   double falsePositiveProbability,
+											   Map<Byte, Integer> sizeOfBloomFilters)
 	{
-		Configuration conf = new Configuration();
-		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-		if (otherArgs.length < 3) {
-			System.err.println("Usage: BloomFilterTester <dataset file for testing> " +
-					"<file with the bloom filters> <output file>");
-			System.exit(1);
+		// Set bloomFilter parameters
+		configuration.setInt(
+				"bloom.filter.number", // how many bloom filter must be created
+				sizeOfBloomFilters.size()
+		);
+
+		int i = 0;
+		for (Map.Entry<Byte, Integer> entry : sizeOfBloomFilters.entrySet()) {
+			configuration.setInt(
+					"bloom.filter.size.key." + i, // rating key of the k-th bloom filter
+					entry.getKey()
+			);
+			configuration.setInt(
+					"bloom.filter.size.value." + i, // size of k-th bloom filter
+					entry.getValue()
+			);
+			i++;
 		}
 
+		configuration.setInt(
+				"bloom.filter.hash", // how many hash functions for each bloom filter
+				computeNumberOfHashFunctions(falsePositiveProbability)
+		);
+
+		System.out.println("Number of hash functions: " + computeNumberOfHashFunctions(falsePositiveProbability));
+		System.out.println("Rating -> bloom filter size: " + sizeOfBloomFilters);
+	}
+
+
+	/**
+	 * TODO
+	 * @param configuration
+	 * @param falsePositiveProbability
+	 * @param input_dataset
+	 * @param output_bloom_filter
+	 * @param output_tester
+	 * @return
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 * @throws InterruptedException
+	 */
+	private static boolean runBloomFilterTester (Configuration configuration,
+												  double falsePositiveProbability,
+												  Path input_dataset,
+												  Path output_bloom_filter,
+												  Path output_tester,
+												  Map<Byte, Integer> sizeOfBloomFilters)
+			throws IOException, ClassNotFoundException, InterruptedException
+	{
 
 		// Create MapReduce job
-		Job job = Job.getInstance(conf, "BloomFilter Tester");
+		Job job = Job.getInstance(configuration, "BloomFilter Tester");
+		generateConfiguration(job.getConfiguration(), falsePositiveProbability, sizeOfBloomFilters);
+
 		job.setJarByClass(BloomFilterTester.class);
 
 
 		// Configure mapper which distributes the dataset
 		MultipleInputs.addInputPath(
 				job,
-				new Path(args[0]),
+				input_dataset,
 				NLineInputFormat.class,
 				BloomFilterMapper.class // Reuse mapper for building bloom filter
 		);
@@ -53,7 +125,7 @@ public class BloomFilterTester {
 		// Configure mapper which distributes the bloom filters
 		MultipleInputs.addInputPath(
 				job,
-				new Path(args[1]),
+				output_bloom_filter,
 				// TODO multiple files??? (probably not: mapreduce automatically handles the partition)
 				SequenceFileInputFormat.class,
 				MapperTesterForBloomFilters.class
@@ -73,9 +145,83 @@ public class BloomFilterTester {
 
 		// Configure output path
 		job.setOutputFormatClass(TextOutputFormat.class);
-		TextOutputFormat.setOutputPath(job, new Path(args[2]));
+		TextOutputFormat.setOutputPath(job, output_tester);
 
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		return job.waitForCompletion(true);
+	}
+
+
+	/**
+	 * TODO
+	 * @param file
+	 * @param falsePositiveProbability
+	 * @throws IOException
+	 */
+	private static Map<Byte, Integer> getBloomFiltersSizeParameters (Configuration conf, Path file,
+																	 Double falsePositiveProbability)
+			throws IOException
+	{
+		// Map each rating to the size its corresponding bloom filter
+		Map<Byte, Integer> sizeOfBloomFilters = new HashMap<>();
+
+		// Open file in HDFS
+		try (BufferedReader br = new BufferedReader(
+				new InputStreamReader(
+						file.getFileSystem(conf).open(file)
+				)
+		)
+		){
+			String line;
+
+			// Read each record of the dataset
+			while ((line = br.readLine()) != null) {
+				// Split the record
+				String[] splits = line.split("\\s+");
+
+				// Parse the record and put it in the map
+				sizeOfBloomFilters.put(
+						Byte.parseByte(splits[0]),
+						computeSizeOfBloomFilter(falsePositiveProbability, Integer.parseInt(splits[1]))
+				);
+			}
+
+		}
+
+		System.out.println("Bloom filter input data = " + sizeOfBloomFilters);
+		return sizeOfBloomFilters;
+	}
+
+	public static void main (String[] args) 
+				throws IOException, InterruptedException, ClassNotFoundException
+	{
+		Configuration configuration = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(configuration, args).getRemainingArgs();
+		if (otherArgs.length < 5) {
+			System.err.println("Usage: BloomFilterTester <false positive p> <dataset file for testing> " +
+					"<file with the bloom filters> <output file> <linecount file>");
+			System.exit(1);
+		}
+
+		double falsePositiveProbability = Double.parseDouble(otherArgs[0]);
+		Path input_dataset = new Path(otherArgs[1]);
+		Path output_bloom_filter = new Path(otherArgs[2]);
+		Path output_tester = new Path(otherArgs[3]);
+		Path linecount_output = new Path(otherArgs[4]); // ?
+
+		// Read file
+		Map<Byte, Integer> sizeOfBloomFilters =
+				getBloomFiltersSizeParameters(configuration, linecount_output, falsePositiveProbability);
+
+		boolean succeded = runBloomFilterTester(
+				configuration,
+				falsePositiveProbability,
+				input_dataset, output_bloom_filter, output_tester,
+				sizeOfBloomFilters);
+
+		if (!succeded) {
+			System.err.println("BloomFilter Tester failed");
+			System.exit(1);
+		}
 	}
 
 }
--- hadoop-bloom-filter/src/main/java/it/unipi/hadoop/bloomfilter/tester/ReducerTester.java
+++ hadoop-bloom-filter/src/main/java/it/unipi/hadoop/bloomfilter/tester/ReducerTester.java
@@ -1,44 +1,66 @@
 package it.unipi.hadoop.bloomfilter.tester;
 
+import it.unipi.hadoop.bloomfilter.builder.BloomFilterUtils;
 import it.unipi.hadoop.bloomfilter.writables.BooleanArrayWritable;
 import it.unipi.hadoop.bloomfilter.writables.IntArrayWritable;
 import it.unipi.hadoop.bloomfilter.writables.GenericObject;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
 
 public class ReducerTester
 		extends Reducer<ByteWritable, GenericObject, ByteWritable, DoubleWritable>
 {
+
+	private static final Logger LOGGER = LogManager.getLogger(ReducerTester.class);
+
 	// Size of the bloom filter to be read from the configuration
-	private int BLOOM_FILTER_SIZE;
+	//private int BLOOM_FILTER_SIZE;
+
+	// size of each BloomFilter
+	private Map<Byte, Integer> BLOOM_FILTER_SIZE;
+
 	private final DoubleWritable SERIALIZABLE_FALSE_POSITIVE = new DoubleWritable();
 
+
 	@Override
-	public void setup(Context context){
-		BLOOM_FILTER_SIZE = context.getConfiguration().getInt("bloom.filter.size", 0);
+	public void setup(Context context) {
+		//BLOOM_FILTER_SIZE = context.getConfiguration().getInt("bloom.filter.size", 0);
+		BLOOM_FILTER_SIZE = BloomFilterUtils.readConfigurationBloomFiltersSize(context.getConfiguration());
+		LOGGER.info("BloomFilter size: " + BLOOM_FILTER_SIZE);
 	}
 
 	@Override
 	public void reduce (ByteWritable key, Iterable<GenericObject> values, Context context)
-			throws IOException, InterruptedException{
+			throws IOException, InterruptedException {
+
+		LOGGER.info("ReducerKey: " + key);
 
 		// Declare the bloomFilter
-		BooleanWritable[] bloomFilter = null;
+		BooleanWritable[] bloomFilter = null; /*new BooleanWritable[BLOOM_FILTER_SIZE.get(key.get())];
+		for (int i = 0; i < bloomFilter.length; i++) {
+			bloomFilter[i] = new BooleanWritable(true);
+		}*/
 
 		// Get the bloom filter from the input values
 		for (GenericObject object : values) {
 			if (object.get() instanceof BooleanArrayWritable) {
 				BooleanArrayWritable booleanArrayWritable = (BooleanArrayWritable) object.get();
 				bloomFilter = (BooleanWritable[]) booleanArrayWritable.toArray();
+				LOGGER.info("bloomFilter = " + Arrays.toString(bloomFilter));
 				break;
 			}
 		}
 
 		// Check if bloom filter was founded
 		if (bloomFilter == null) {
-			System.err.println("[TEST-REDUCER]: Bloom filter " + key + "doesn't exists");
+			LOGGER.info("[REDUCER-TEST]: BloomFilter " + key + " doesn't exist");
+			//System.err.println("[TEST-REDUCER]: Bloom filter " + key + "doesn't exist");
 			return;
 		}
 
@@ -62,9 +84,11 @@ public class ReducerTester
 			for (IntWritable i : intArray) {
 				int index = i.get();
 
-				if(index >= BLOOM_FILTER_SIZE){
-					System.err.println("[TEST-REDUCER]: Index " + index +
-							" out of bound for bloom filter " + key.get());
+				if (index < 0 || index >= BLOOM_FILTER_SIZE.get(key.get())) {
+					LOGGER.warn("[Reducer-Test]: index " + index + " for key " + key +
+							" not valid - out of bound");
+					/*System.err.println("[TEST-REDUCER]: Index " + index +
+							" out of bound for bloom filter " + key.get());*/
 					return;
 				}
 
@@ -83,10 +107,13 @@ public class ReducerTester
 				numberOfFalsePositives++;
 			}
 
+			LOGGER.info("#tests: " + numberOfTests);
+			LOGGER.info("#falsePositive: " + numberOfFalsePositives);
 		}
 
 		SERIALIZABLE_FALSE_POSITIVE.set(numberOfFalsePositives / numberOfTests);
 		context.write(key, SERIALIZABLE_FALSE_POSITIVE);
+		LOGGER.info("%falsePositive: " + SERIALIZABLE_FALSE_POSITIVE.get());
 	}
 
 }
