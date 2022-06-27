09050830eb4b508c01b1134fc147947e1d2eb607
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2022-06-27 11:01:46 +0200

    Reindent according to `PEP 8`

--- spark-bloom-filter/bloomfilters_builder.py
+++ spark-bloom-filter/bloomfilters_builder.py
@@ -4,67 +4,67 @@ from pyspark import SparkContext
 
 
 def parse_arguments():
-	"""
-	Parse the command line input arguments
-	:return: parsed arguments
-	"""
-	parser = argparse.ArgumentParser()
-	parser.add_argument('false_positive_prob', type=str, help='probability of false positives')
-	parser.add_argument('dataset_input_file', type=str, help='path of the dataset')
-	parser.add_argument('linecount_file', type=str, help='path of the linecount output file')
-	parser.add_argument('output_file', type=str, help='path of the output file')
-	args = parser.parse_args()
-	
-	return args.false_positive_prob, args.dataset_input_file, args.linecount_file, args.output_file
+    """
+    Parse the command line input arguments
+    :return: parsed arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('false_positive_prob', type=str, help='probability of false positives')
+    parser.add_argument('dataset_input_file', type=str, help='path of the dataset')
+    parser.add_argument('linecount_file', type=str, help='path of the linecount output file')
+    parser.add_argument('output_file', type=str, help='path of the output file')
+    args = parser.parse_args()
+    
+    return args.false_positive_prob, args.dataset_input_file, args.linecount_file, args.output_file
 
 
 def initialize_bloom_filters_array():
-	"""
-	Instantiate the temporary Bloom Filters with all values to False
-	:return: the Bloom Filters created
-	"""
-	bloom_filters = dict()
-	for i, j in broadcast_size_of_bloom_filters.value:
-		bloom_filters[i] = [False for _ in range(j)]
-	return bloom_filters
+    """
+    Instantiate the temporary Bloom Filters with all values to False
+    :return: the Bloom Filters created
+    """
+    bloom_filters = dict()
+    for i, j in broadcast_size_of_bloom_filters.value:
+        bloom_filters[i] = [False for _ in range(j)]
+    return bloom_filters
 
 
 def set_bloom_filter(bloom_filter_hashes):
-	"""
-	Set to True the corresponding item of the Bloom Filter
-	:param bloom_filter_hashes: K-V pair (rating, [hashes]), where the hash values are the index to set in the filter
-	:return: the Bloom filters structure
-	"""
-	for i in bloom_filter_hashes[1]:
-		bloom_filters[bloom_filter_hashes[0]][i] = True
-	return bloom_filters
+    """
+    Set to True the corresponding item of the Bloom Filter
+    :param bloom_filter_hashes: K-V pair (rating, [hashes]), where the hash values are the index to set in the filter
+    :return: the Bloom filters structure
+    """
+    for i in bloom_filter_hashes[1]:
+        bloom_filters[bloom_filter_hashes[0]][i] = True
+    return bloom_filters
 
 
 if __name__ == '__main__':
-	false_positive_prob, dataset_input_file, linecount_file, output_file = parse_arguments()
-	
-	sc = SparkContext(appName="BLOOM_FILTER", master="yarn")
-	sc.addPyFile("bloomfilters_util.py")  # Add dependency
-	
-	broadcast_hash_function_number = sc.broadcast(util.compute_number_of_hash_functions(false_positive_prob))
-	broadcast_size_of_bloom_filters = sc.broadcast(util.get_size_of_bloom_filters(sc, linecount_file, false_positive_prob))
-	bloom_filters = initialize_bloom_filters_array()
-	
-	# map => (rating,posToSet) => x[0] = rating , x[1] = posToSet
-	
-	"""
-		1. read dataset
-		2. map: split each line to extract [movieId, averageRating]
-		3. map: round averageRating to the closest integer and output the array of hashes of the movie's id
-		4. reduceByKey: group by rating and create an unique list of all hash values computed in the previous step
-		5. map: take (rating, [hashes]) and create the bloom filter setting to True the corresponding item of the array
-		6. save the results (the Bloom Filter) as a text file
-	"""
-	# todo remove reduceByKey stage ?
-	sc.textFile(dataset_input_file) \
-		.map(lambda x: x.split('\t')[0:2]) \
-		.map(lambda x: (int(round(float(x[1]))),
-	                    util.compute_hashes(x, broadcast_size_of_bloom_filters, broadcast_hash_function_number))) \
-		.reduceByKey(lambda x, y: list(set(x + y))) \
-		.map(lambda x: (x[0], set_bloom_filter(x))) \
-		.saveAsObjectFile(output_file)
+    false_positive_prob, dataset_input_file, linecount_file, output_file = parse_arguments()
+    
+    sc = SparkContext(appName="BLOOM_FILTER", master="yarn")
+    sc.addPyFile("bloomfilters_util.py")  # Add dependency
+    
+    broadcast_hash_function_number = sc.broadcast(util.compute_number_of_hash_functions(false_positive_prob))
+    broadcast_size_of_bloom_filters = sc.broadcast(util.get_size_of_bloom_filters(sc, linecount_file, false_positive_prob))
+    bloom_filters = initialize_bloom_filters_array()
+    
+    # map => (rating,posToSet) => x[0] = rating , x[1] = posToSet
+    
+    """
+        1. read dataset
+        2. map: split each line to extract [movieId, averageRating]
+        3. map: round averageRating to the closest integer and output the array of hashes of the movie's id
+        4. reduceByKey: group by rating and create an unique list of all hash values computed in the previous step
+        5. map: take (rating, [hashes]) and create the bloom filter setting to True the corresponding item of the array
+        6. save the results (the Bloom Filter) as a text file
+    """
+    # todo remove reduceByKey stage ?
+    sc.textFile(dataset_input_file) \
+        .map(lambda x: x.split('\t')[0:2]) \
+        .map(lambda x: (int(round(float(x[1]))),
+                        util.compute_hashes(x, broadcast_size_of_bloom_filters, broadcast_hash_function_number))) \
+        .reduceByKey(lambda x, y: list(set(x + y))) \
+        .map(lambda x: (x[0], set_bloom_filter(x))) \
+        .saveAsObjectFile(output_file)
--- spark-bloom-filter/bloomfilters_util.py
+++ spark-bloom-filter/bloomfilters_util.py
@@ -3,54 +3,54 @@ import mmh3
 
 
 def compute_hashes(line, size_of_bloom_filters, hash_function_number):
-	"""
-	Compute the hash values of a movie (MurmurHash functions applied using different seeds)
-	
-	:param line: single line of the dataset in the format [movieId, averageRating]
-	:param size_of_bloom_filters: array with the size of each bloom filter
-	:param hash_function_number: how many hash functions must be used
-	
-	:return: array of computed hash values
-	"""
-	movie_id = line[0]
-	bloom_filter_size = size_of_bloom_filters[int(round(float(line[1])))][1]
-	return [mmh3.hash(movie_id, i) % bloom_filter_size for i in range(hash_function_number.value)]
+    """
+    Compute the hash values of a movie (MurmurHash functions applied using different seeds)
+    
+    :param line: single line of the dataset in the format [movieId, averageRating]
+    :param size_of_bloom_filters: array with the size of each bloom filter
+    :param hash_function_number: how many hash functions must be used
+    
+    :return: array of computed hash values
+    """
+    movie_id = line[0]
+    bloom_filter_size = size_of_bloom_filters[int(round(float(line[1])))][1]
+    return [mmh3.hash(movie_id, i) % bloom_filter_size for i in range(hash_function_number.value)]
 
 
 def compute_number_of_hash_functions(false_positive_prob):
-	"""
-	Compute how many hash functions are required for the bloom filter
-	
-	:param false_positive_prob: desired probability of a false positive
-	
-	:return: the number of hash functions
-	"""
-	return int(round(- math.log(false_positive_prob) / math.log(2)))
+    """
+    Compute how many hash functions are required for the bloom filter
+    
+    :param false_positive_prob: desired probability of a false positive
+    
+    :return: the number of hash functions
+    """
+    return int(round(- math.log(false_positive_prob) / math.log(2)))
 
 
 def compute_size_of_bloom_filter(false_positive_prob, number_of_inputs):
-	"""
-	Compute the size of a bloom filter in order to meet the required false positive probability
-	
-	:param false_positive_prob: desired probability of a false positive
-	:param number_of_inputs: estimated number of key that will be inserted in the bloom filter
-	
-	:return: the size of a single bloom filter
-	"""
-	return int(round(-(number_of_inputs * math.log(false_positive_prob)) / math.pow(math.log(2), 2)))
+    """
+    Compute the size of a bloom filter in order to meet the required false positive probability
+    
+    :param false_positive_prob: desired probability of a false positive
+    :param number_of_inputs: estimated number of key that will be inserted in the bloom filter
+    
+    :return: the size of a single bloom filter
+    """
+    return int(round(-(number_of_inputs * math.log(false_positive_prob)) / math.pow(math.log(2), 2)))
 
 
 def get_size_of_bloom_filters(sc, linecount_file, false_positive_prob):
-	"""
-	Retrieve the size of each Bloom Filter
-	
-	:param sc: Spark context
-	:param linecount_file: name of the file with the number of keys for each rating
-	:param false_positive_prob: desired probability of a false positive
-	
-	:return: an array of tuple containing the size of all Bloom Filters in the format (rating, size)
-	"""
-	return sc.textFile(linecount_file) \
-		.map(lambda x: x.split('\t')[0:2]) \
-		.map(lambda x: (x[0], compute_size_of_bloom_filter(false_positive_prob, x[1]))) \
-		.collect()
+    """
+    Retrieve the size of each Bloom Filter
+    
+    :param sc: Spark context
+    :param linecount_file: name of the file with the number of keys for each rating
+    :param false_positive_prob: desired probability of a false positive
+    
+    :return: an array of tuple containing the size of all Bloom Filters in the format (rating, size)
+    """
+    return sc.textFile(linecount_file) \
+        .map(lambda x: x.split('\t')[0:2]) \
+        .map(lambda x: (x[0], compute_size_of_bloom_filter(false_positive_prob, x[1]))) \
+        .collect()
