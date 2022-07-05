b557a8cfcfab01138464f1dcc4a4111af9496e81
veronicator <80213270+veronicator@users.noreply.github.com>
2022-07-05 20:39:12 +0200

    [Hadoop] added doc to ConfigurationName

--- hadoop-bloom-filter/src/main/java/it/unipi/hadoop/bloomfilter/util/BloomFilterConfigurationName.java
+++ hadoop-bloom-filter/src/main/java/it/unipi/hadoop/bloomfilter/util/BloomFilterConfigurationName.java
@@ -1,5 +1,9 @@
 package it.unipi.hadoop.bloomfilter.util;
 
+/**
+ * Enumerator type containing the "hardcoded" name for the configuration parameters,
+ * in order to avoid to use directly the parameter name inside the code
+ */
 public enum BloomFilterConfigurationName {
     NUMBER("bloom.filter.number"),
     RATING_KEY("bloom.filter.size.key"),
